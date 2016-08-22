3a26fa681fa7e308359693a96b134da495a35b21
Andreas Tietz <andreas.tietz@gmail.com>
2016-08-22 22:00:32 +0200

    Removed commented out implementation with promises.

--- scraper.js
+++ scraper.js
@@ -3,22 +3,6 @@ const fetch = require('node-fetch');
 const cheerio = require('cheerio');
 const fs = require('fs');
 
-// fetch('http://unicode.org/emoji/charts/full-emoji-list.html')
-// .then((res) => res.text())
-// .then((html) => cheerio.load(html))
-// .then(($) => {
-// 	return $('td.rchars').map(function () {
-// 		const tr = $(this).parent();
-// 		return {
-// 			chars: $('.chars', tr).text(),
-// 			name: $('.name', tr).eq(0).text(),
-// 			keywords: $('.name', tr).eq(1).text()
-// 		}
-// 	}).get();
-// })
-// .then((emojis) => JSON.stringify(emojis, null, 2))
-// .then((text) => fs.writeFileSync('emojis.json', text));
-
 co(function *() {
 	const res = yield fetch('http://unicode.org/emoji/charts/full-emoji-list.html');
 	const html = yield res.text();
