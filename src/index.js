54edc308a6473485d48851c9b3ca8cbc82c95edf
Andreas Tietz <andreas.tietz@gmail.com>
2016-09-11 21:18:54 +0200

    Naively implemented generating a minified variant of the emoji.json file.

--- src/index.js
+++ src/index.js
@@ -42,4 +42,39 @@ co(function* () {
 		...emojiZwjSequences.zwjEmoji,
 	];
 	fs.writeFileSync('lib/emoji.json', JSON.stringify(combined, null, 2));
+	const minified = combined.map(datum => {
+		if (datum.presentation) {
+			if (datum.presentation.default) {
+				datum.presentation.default = datum.presentation.default.sequence;
+			}
+			if (datum.presentation.variation) {
+				if (datum.presentation.variation.text) {
+					datum.presentation.variation.text = datum.presentation.variation.text.sequence;
+				}
+				if (datum.presentation.variation.emoji) {
+					datum.presentation.variation.emoji = datum.presentation.variation.emoji.sequence;
+				}
+			}
+			if (datum.combination && datum.combination.keycap && datum.combination.keycap.presentation) {
+				if (datum.combination.keycap.presentation.default) {
+					datum.combination.keycap.presentation.default = datum.combination.keycap.presentation.default.sequence;
+				}
+				if (datum.combination.keycap.presentation.variation) {
+					if (datum.combination.keycap.presentation.variation.text) {
+						datum.combination.keycap.presentation.variation.text = datum.combination.keycap.presentation.variation.text.sequence;
+					}
+					if (datum.combination.keycap.presentation.variation.emoji) {
+						datum.combination.keycap.presentation.variation.emoji = datum.combination.keycap.presentation.variation.emoji.sequence;
+					}
+				}
+			}
+			if (datum.modification && datum.modification.skin) {
+				datum.modification.skin = Object.keys(datum.modification.skin).map(skinType =>
+					datum.modification.skin[skinType].sequence
+				);
+			}
+		}
+		return datum;
+	});
+	fs.writeFileSync('lib/emoji.min.json', JSON.stringify(combined));
 });
