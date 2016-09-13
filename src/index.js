b02a1f438ae6084dd86bef4c2e2c083faa94082e
Andreas Tietz <andreas.tietz@gmail.com>
2016-09-14 00:18:55 +0200

    Readability

--- src/index.js
+++ src/index.js
@@ -12,35 +12,44 @@ process.on('uncaughtException', (err) => { throw err; });
 process.on('unhandledRejection', (err) => { throw err; });
 
 co(function* main() {
+
 	const unicodeData = yield generateUnicodeData({
 		url: 'http://www.unicode.org/Public/9.0.0/ucd/UnicodeData.txt',
 	});
+
 	const emojiSources = yield generateEmojiSources({
 		url: 'http://unicode.org/Public/9.0.0/ucd/EmojiSources.txt',
 	});
+
 	const emojiSequences = yield generateEmojiSequences({
 		url: 'http://www.unicode.org/Public/emoji/3.0/emoji-sequences.txt',
 		getNameForCodepoint: unicodeData.getNameForCodepoint,
 	});
+
 	const standardizedVariants = yield generateStandardizedVariants({
 		url: 'http://unicode.org/Public/9.0.0/ucd/StandardizedVariants.txt',
 	});
+
 	const emojiZwjSequences = yield generateEmojiZwjSequences({
 		url: 'http://www.unicode.org/Public/emoji/4.0/emoji-zwj-sequences.txt',
 		getNameForCodepoint: unicodeData.getNameForCodepoint,
 	});
+
 	const emojiData = yield generateEmojiData({
 		url: 'http://www.unicode.org/Public/emoji/3.0/emoji-data.txt',
 		getNameForCodepoint: unicodeData.getNameForCodepoint,
 		getVariationSequencesForCodepoint: standardizedVariants.getVariationSequencesForCodepoint,
 		getCombinationsForCodepoint: emojiSequences.getCombinationsForCodepoint,
 		getShiftJisCodeByCarrierForCodepoint: emojiSources.getShiftJisCodeByCarrierForCodepoint,
+
 	});
+
 	const combined = [
 		...emojiData.emoji,
 		...emojiSequences.flagEmoji,
 		...emojiZwjSequences.zwjEmoji,
 	];
+
 	fs.writeFileSync('lib/emoji.json', JSON.stringify(combined, null, 2));
 
 	const minifyDatum = (parentNode, nodeKey) => {
@@ -58,5 +67,6 @@ co(function* main() {
 	};
 
 	const minified = combined.map(datum => minifyDatum(datum));
+
 	fs.writeFileSync('lib/emoji.min.json', JSON.stringify(minified));
 });
