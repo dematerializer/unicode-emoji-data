11b3bb618df5273999aedd193ee04c01960fdcef
Andreas Tietz <andreas.tietz@gmail.com>
2016-09-26 21:44:34 +0200

    Testing unicode-data

---
+++ src/specs/test/unicode-data.test.js
@@ -0,0 +1,22 @@
+/* eslint-env node, mocha */
+import 'babel-polyfill';
+import { expect } from 'chai';
+import { internals } from '../unicode-data';
+
+describe('unicode-data', () => {
+	it('should use a reasonable default url', () => {
+		expect(internals.defaultUrl).to.equal('http://unicode.org/Public/9.0.0/ucd/UnicodeData.txt');
+	});
+	it('should transform data to map each code point to a name', () => {
+		const data = [
+			{ codepoint: '1F600', name: 'GRINNING FACE', comment: 'foo' },
+			{ codepoint: '1F601', name: 'GRINNING FACE WITH SMILING EYES', comment: 'bar' },
+		];
+		const expected = {
+			'1F600': 'GRINNING FACE',
+			'1F601': 'GRINNING FACE WITH SMILING EYES',
+		};
+		const nameForCodepoint = internals.buildNameForCodepoint(data);
+		expect(nameForCodepoint).to.deep.equal(expected);
+	});
+});
