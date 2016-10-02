6e06aed8552a45dcf028f0a644b98738eadec4fe
Andreas Tietz <andreas.tietz@gmail.com>
2016-10-02 21:13:48 +0200

    Tested emoji-sources

--- src/test/emoji-sources.test.js
+++ src/test/emoji-sources.test.js
@@ -1,6 +1,5 @@
-/* eslint-disable no-unused-vars */
-
-import { internals } from '../emoji-sources';
+import fetchMock from 'fetch-mock';
+import buildEmojiSources, { internals } from '../emoji-sources';
 
 const {
 	defaultUrl,
@@ -10,5 +9,43 @@ const {
 describe('emoji-sources', () => {
 	it('should use a reasonable default url', () => {
 		expect(defaultUrl).to.equal('http://unicode.org/Public/9.0.0/ucd/EmojiSources.txt');
+	});
+	it('should map each unicode code point or sequence to one or more Shift-JIS codes for cell phone carrier symbols', () => {
+		const data = [
+			{ unicode: '00AE', docomo: 'F9DB', kddi: 'F775', softbank: 'F7EF' },
+			{ unicode: '2139', docomo: '', kddi: 'F74F', softbank: '' },
+			{ unicode: '2754', docomo: '', kddi: '', softbank: 'F9D6' },
+			{ unicode: 'bogus', docomo: '', kddi: '', softbank: '' },
+		];
+		const expected = {
+			'00AE': {
+				docomo: 'F9DB', kddi: 'F775', softbank: 'F7EF',
+			},
+			'2139': { // eslint-disable-line quote-props
+				docomo: undefined, kddi: 'F74F', softbank: undefined,
+			},
+			'2754': { // eslint-disable-line quote-props
+				docomo: undefined, kddi: undefined, softbank: 'F9D6',
+			},
+		};
+		const shiftJisCodesForCodepoint = buildShiftJisCodesForCodepoint(data);
+		expect(shiftJisCodesForCodepoint).to.deep.equal(expected);
+	});
+	it('should generate an API', (done) => {
+		fetchMock.get('*', `
+			00AE;F9DB;F775;F7EF
+			2139;;F74F;
+			2754;;;F9D6
+		`);
+		const step = buildEmojiSources({});
+		step.next().value.then((content) => { // wait until first yield's promise (mocked fetch) resolves
+			const api = step.next(content).value; // manually hand over mocked content to the left side of yield
+			expect(api).to.have.all.keys('getShiftJisCodesForCodepoint');
+			expect(api.getShiftJisCodesForCodepoint).to.be.a('function');
+			expect(api.getShiftJisCodesForCodepoint('00AE')).to.deep.equal({
+				docomo: 'F9DB', kddi: 'F775', softbank: 'F7EF',
+			});
+			done();
+		});
 	});
 });
