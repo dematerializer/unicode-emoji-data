ee95d1bc2bc9594a60317bb899b2ec725b286a7d
Andreas Tietz <andreas.tietz@gmail.com>
2016-10-05 23:16:18 +0200

    Better exception handling in tests

--- test-env.js
+++ test-env.js
@@ -2,3 +2,6 @@ import 'babel-polyfill';
 import { expect } from 'chai';
 
 global.expect = expect;
+
+process.on('uncaughtException', (err) => { throw err; });
+process.on('unhandledRejection', (err) => { throw err; });
