c550468e5b07a56bdafd0c47540ed8c0fa1213b1
Andreas Tietz <andreas.tietz@gmail.com>
2016-11-07 00:19:44 +0100

    Simplified stuff

--- src/index.js
+++ src/index.js
@@ -1,22 +1,17 @@
-const expandEmojiData = require('./expand-emoji-data.js');
+import expandEmojiData from './expand-emoji-data';
+import emojiDataV3 from './emoji-data-v3.json';
+import emojiDataV4 from './emoji-data-v4.json';
 
-const emojiData = {
-	v3: require('./emoji-data-v3.json'), // eslint-disable-line global-require
-	v4: require('./emoji-data-v4.json'), // eslint-disable-line global-require
-};
-
-const expandedEmojiData = {
-	v3: expandEmojiData(emojiData.v3),
-	v4: expandEmojiData(emojiData.v4),
-};
+const expandedEmojiDataV3 = expandEmojiData(emojiDataV3);
+const expandedEmojiDataV4 = expandEmojiData(emojiDataV4);
 
 export default {
 	v3: {
-		full: emojiData.v3,
-		expanded: expandedEmojiData.v3,
+		full: emojiDataV3,
+		expanded: expandedEmojiDataV3,
 	},
 	v4: {
-		full: emojiData.v4,
-		expanded: expandedEmojiData.v4,
+		full: emojiDataV4,
+		expanded: expandedEmojiDataV4,
 	},
 };
