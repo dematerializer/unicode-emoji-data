7a8dcf628238fdc8656148b592248050f00d8cf8
Andreas Tietz <andreas.tietz@gmail.com>
2016-12-01 21:32:54 +0100

    Simplified API

--- src/index.js
+++ src/index.js
@@ -2,16 +2,9 @@ import expandEmojiData from './expand-emoji-data';
 import emojiDataV3 from '../res/emoji-data-v3.json';
 import emojiDataV4 from '../res/emoji-data-v4.json';
 
-const expandedEmojiDataV3 = expandEmojiData(emojiDataV3);
-const expandedEmojiDataV4 = expandEmojiData(emojiDataV4);
-
 export default {
-	v3: {
-		full: emojiDataV3,
-		expanded: expandedEmojiDataV3,
-	},
-	v4: {
-		full: emojiDataV4,
-		expanded: expandedEmojiDataV4,
-	},
+	expand: expandEmojiData,
+	v3: emojiDataV3,
+	v4: emojiDataV4,
+	latest: emojiDataV4,
 };
